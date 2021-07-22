099850402a185865e26a700a827d395882f4a8de
Moha <mohacyr@os.city>
2021-07-21 21:08:02 -0500

    feat: add TicTacToeBtns component
    Se agregó el componente con 9 botones que al darles click muestran el número del boton en consola
    
    Refs #7

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import './App.css';
-import { FaRegTimesCircle, FaRegCircle} from "react-icons/fa";
+import "./App.css";
+import { FaRegTimesCircle, FaRegCircle } from "react-icons/fa";
+import TicTacToeBtns from "./Components/TicTacToeBtns";
 
 const App = () => {
   return (
     <div id="main">
-      <FaRegTimesCircle size="150"/>
-      <FaRegCircle size="150"/>
+      <FaRegTimesCircle size="150" />
+      <FaRegCircle size="150" />
+      <TicTacToeBtns />
     </div>
   );
-}
+};
 
 export default App;
