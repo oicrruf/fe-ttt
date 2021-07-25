2ee3987d1a918bb53edd3a9b30693038f30c85ff
Víctor M. Reyes <victor@devf.mx>
2021-07-25 13:26:16 -0600

    fix: error in pull integration

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import { TicTacToeBtns } from ".";
+
+const Home = () => {
+  return (
+    <>
+      <div className="bg-gray-50">
+        <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+          <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+            <span className="block">Tic Tac Toe Game</span>
+            <span className="block text-indigo-600">Master en Code - G4</span>
+          </h2>
+        </div>
+      </div>
+      <TicTacToeBtns />
+    </>
+  );
+};
+
+export { Home };
--- src/components/TicTacToeBtns.js
+++ src/components/TicTacToeBtns.js
@@ -1,19 +1,30 @@
-import React from "react";
+import { FaRegTimesCircle, FaRegCircle } from "react-icons/fa";
+
 const selectRegion = (id) => {
   console.log(id);
 };
 const TicTacToeBtns = () => {
   return (
     <div>
-      <button onClick={() => selectRegion(1)}>1</button>
-      <button onClick={() => selectRegion(2)}>2</button>
-      <button onClick={() => selectRegion(3)}>3</button>
-      <button onClick={() => selectRegion(4)}>4</button>
-      <button onClick={() => selectRegion(5)}>5</button>
-      <button onClick={() => selectRegion(6)}>6</button>
-      <button onClick={() => selectRegion(7)}>7</button>
-      <button onClick={() => selectRegion(8)}>8</button>
-      <button onClick={() => selectRegion(9)}>9</button>
+      <table>
+        <tbody>
+          <tr>
+            <td><button onClick={() => selectRegion(1)}><FaRegTimesCircle size="50" /></button></td>
+            <td><button onClick={() => selectRegion(2)}><FaRegCircle size="50" /></button></td>
+            <td><button onClick={() => selectRegion(3)}><FaRegTimesCircle size="50" /></button></td>
+          </tr>
+          <tr>
+            <td><button onClick={() => selectRegion(4)}><FaRegCircle size="50" /></button></td>
+            <td><button onClick={() => selectRegion(5)}><FaRegTimesCircle size="50" /></button></td>
+            <td><button onClick={() => selectRegion(6)}><FaRegCircle size="50" /></button></td>
+          </tr>
+          <tr>
+            <td><button onClick={() => selectRegion(7)}><FaRegCircle size="50" /></button></td>
+            <td><button onClick={() => selectRegion(8)}><FaRegCircle size="50" /></button></td>
+            <td><button onClick={() => selectRegion(9)}><FaRegTimesCircle size="50" /></button></td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
