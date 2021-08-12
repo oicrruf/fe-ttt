b35b6e7d991cb6697346c9d82ac1c100eb92a192
Daniel <danielsquinteros@gmail.com>
2021-08-11 23:54:07 -0400

    fix: text in component home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,7 @@
 import React from "react";
-import {
-  useParams
-} from "react-router-dom";
 import { TicTacToeBtns } from "./TicTacToeBtns";
 
 const Home = () => {
-  const { time, dev } = useParams();
 
   return (
     <>
@@ -13,7 +9,7 @@ const Home = () => {
         <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
           <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
             <span className="block">Tic Tac Toe Game</span>
-            <span className="block text-indigo-600">{time}{dev}</span>
+            <span className="block text-indigo-600">Master en Code - G4</span>
           </h2>
         </div>
       </div>
