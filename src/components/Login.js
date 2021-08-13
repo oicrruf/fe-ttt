6b855b01af8a505d6dfb11902f7daf775ebcf30c
Charlie <carlos.reyesma@udlap.mx>
2021-08-12 19:58:33 -0500

    fix/formika con validacion

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,15 +10,27 @@ function Login() {
         nombre:'',
         email:''
       }}
+      validate={(valores)=>{
+        let errores={};
+        if(!valores.nombre){
+          errores.nombre='Por favor ingresa un nombre'
+          console.log('nombre?');
+        }else if(!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(valores.nombre)){
+          errores.nombre='El nombre solo puede contener letras y espacios';
+        }
+        return errores;
+      }}
       
-      onSubmit={(valores)=>{
+      onSubmit={(valores,{resetForm})=>{
+        resetForm();
         console.log('Formulario enviado');
         console.log(valores.nombre);
       }}
       >
         
-        {({values, handleSubmit, handleChange, handleBlur}) => (
+        {({errors, touched, values, handleSubmit, handleChange, handleBlur}) => (
           <form className="formReg" onSubmit={handleSubmit}>
+            {console.log(errors)}
             <input
               id="nombre"
               className="registroInput"
@@ -28,10 +40,11 @@ function Login() {
               onChange={handleChange}
               onBlur={handleBlur}
             />
-           
+           {touched.nombre && errors.nombre && <div className='error'>{errors.nombre}</div>}
 
             <button className="btnRegistro">Login</button>
             {/*  <Button onClick={submit} /> */}
+
           </form>
         )}
       </Formik>
