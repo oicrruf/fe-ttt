d6152ed18da3a187284ecc15531757e0488cc62f
SabinShadow <mjucarrillo.1990@gmail.com>
2021-08-16 19:54:24 -0600

    se corrio comando prettier y slint

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,6 @@
 import { TicTacToeBtns } from "./TicTacToeBtns";
 import React from "react";
-import Swal from 'sweetalert2'
-
+import Swal from "sweetalert2";
 
 const Home = () => {
   return (
@@ -19,61 +18,45 @@ const Home = () => {
   );
 };
 
+const saveLocalStorage = () => {
+  let nickname = Swal.fire({
+    input: "textarea",
+    inputLabel: "Ingresa nickname si no estas registrado",
+    inputPlaceholder: "Escribe aqui",
+    inputAttributes: {
+      "aria-label": "Escribe aqui",
+    },
+    showCancelButton: true,
+    //timer: "4000"   //cierra automaticamente la ventana
 
+    preConfirm: (login) => {
+      return fetch(`//api.github.com/users/${login}`)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(response.statusText);
+          }
+          return response.json();
+        })
 
-const saveLocalStorage = () =>{
-  
-  let nickname  = Swal.fire( 
-    
-    {
-      
-      input: 'textarea',
-      inputLabel: 'Ingresa nickname si no estas registrado',
-      inputPlaceholder: 'Escribe aqui',
-      inputAttributes: {
-        'aria-label': 'Escribe aqui'
-      },
-      showCancelButton: true,
-      //timer: "4000"   //cierra automaticamente la ventana
-      
-      preConfirm: (login) => {
-        return fetch(`//api.github.com/users/${login}`)
-        
-          .then(response => {
-            if (!response.ok) {
-              throw new Error(response.statusText)
-            }
-            return response.json()
-          })
-          
-          .catch(error => {
-            Swal.resetValidationMessage(
-              `Request failed: ${error}`
-            )
-          })
-          
-      },
-      allowOutsideClick: () => !Swal.isLoading()
-    })
-    .then((resultado) => {
-      if (resultado.value) {
-          let nickname = resultado.value;
-          localStorage.setItem("nickname", JSON.stringify(nickname))
-          Swal.fire({
-            title: `${resultado.value.login}'s avatar`,
-            imageUrl: resultado.value.avatar_url
-          })
+        .catch((error) => {
+          Swal.resetValidationMessage(`Request failed: ${error}`);
+        });
+    },
+    allowOutsideClick: () => !Swal.isLoading(),
+  }).then((resultado) => {
+    if (resultado.value) {
+      let nickname = resultado.value;
+      localStorage.setItem("nickname", JSON.stringify(nickname));
+      Swal.fire({
+        title: `${resultado.value.login}'s avatar`,
+        imageUrl: resultado.value.avatar_url,
+      });
     }
-  })
-    }
-  
-  
+  });
+};
 
-  //console.log(nickname)
+//console.log(nickname)
 
-  
-
-
-saveLocalStorage()
+saveLocalStorage();
 
 export { Home };
